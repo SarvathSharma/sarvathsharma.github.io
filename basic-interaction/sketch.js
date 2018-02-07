58d1cd55e8ceb8548392fc2f217366a99a7c3320
SarvathSharma <sarvath2000@gmail.com>
2018-02-07 15:15:38 -0600

    work

---
+++ basic-interaction/sketch.js
@@ -0,0 +1,27 @@
+// Basic Interaction Demo
+// Sarvath Sharma
+// Feb 7, 2018
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
+
+function keyPressed() {
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(width),random(height), random(100), random(100));
+}
+
+function mouseClicked() {
+  fill(random(255), random(255), random(255), random(255));
+  rect(mouseX, mouseY, random(255), random(255));
+}
+
+function deviceShaken() {
+  fill(random(255), random(255), random(255), random(255));
+  textSize(32);
+  text('Congrats! You did something', 50, 50);
+}
